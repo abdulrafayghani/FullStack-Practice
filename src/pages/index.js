116837c7759f7690834f71f231fa0eeeefba6706
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-10-24 21:56:41 +0500

    basic structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,24 @@
 import React from "react"
+import { useQuery, useMutation } from "@apollo/client"
+import gql from "graphql-tag"
+
+const BookMarksQuery = gql`
+  {
+    bookmark {
+       url
+    }
+  }
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const { loading, error, data } = useQuery(BookMarksQuery)
+  console.log(data)
+
+  return (
+    <div>
+      {" "}
+      {/* hello */}
+      <p>{JSON.stringify(data)}</p>
+    </div>
+  )
 }
